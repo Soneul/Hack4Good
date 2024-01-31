e6d35e079f01845a8be727e388e98d84975173e4
Alvin <soneul.alvin@gmail.com>
2024-01-31 23:57:35 +0800

    Revamp Home Page Template
    Revamp due to change in design idea to show opportunities in the portal instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,79 +1,83 @@
-import * as React from 'react';
-import Link from 'next/link';
-import { AppBar, Toolbar, Typography, Button, Box, Container, Grid, Paper } from '@mui/material';
+import * as React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Box,
+  Grid,
+} from "@mui/material";
+import { Instagram } from "@mui/icons-material";
 
 export default function HomePage() {
   return (
-    <Box>
-      {/* AppBar with navigation links */}
-      <AppBar position="static" color="default" elevation={0}>
+    <Box sx={{ flexGrow: 1 }}>
+      {/* Top navigation bar */}
+      <AppBar position="static" color="transparent" elevation={0}>
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Hack4Good
           </Typography>
-          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link href="/about" passHref>
-              <Button href="#" color="primary" variant="outlined">
-                Log in
-              </Button>
-            </Link>
-            <Button href="#" color="primary" variant="contained">
-              Sign up
-            </Button>
-          </Box>
+          <Button color="inherit">Homepage</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+          <Button color="inherit">Opportunities</Button>
+          <Button color="inherit">Logout</Button>
         </Toolbar>
       </AppBar>
 
-      {/* Main image section */}
-      <Container maxWidth="lg" component="main" sx={{ pt: 8, pb: 6 }}>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          Hack 4 Good's Home Page
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" component="p">
-          Slogan.
-        </Typography>
-      </Container>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          {/* Main content */}
+          <Grid item xs={12} md={8}>
+            <Box sx={{ textAlign: "center", my: 4 }}>
+              <Typography variant="h3" gutterBottom>
+                Hack 4 Goodaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </Typography>
+              <Typography variant="subtitle1" paragraph>
+                Descriptionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </Typography>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Learn More
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Contact
+                  </Button>
+                </Grid>
+              </Grid>
 
-      {/* Action buttons */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} justifyContent="center">
-          <Grid item>
-            <Button variant="contained" color="primary" size="large">
-              Donate
-            </Button>
+              {/* Social media icons */}
+              <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+                <Instagram sx={{ margin: 1 }} />
+              </Box>
+            </Box>
           </Grid>
-          <Grid item>
-            <Button variant="outlined" color="primary" size="large">
-              Volunteer
-            </Button>
+
+          {/* Placeholder for the image */}
+          <Grid item xs={12} md={4}>
+            <Box
+              sx={{
+                width: "100%",
+                height: 300,
+                backgroundColor: "secondary.main",
+                mt: 4,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                typography: "h5",
+              }}
+            >
+              Placeholder for Image/Illustration
+            </Box>
           </Grid>
         </Grid>
       </Container>
-
-      {/* Explore Opportunities section */}
-      <Container maxWidth="md" component="section" sx={{ py: 8 }}>
-      <Typography variant="h4" align="center" color="textPrimary" gutterBottom>
-        Explore Opportunities
-      </Typography>
-      <Grid container spacing={4} justifyContent="center">
-        {/* Temporary items mapped here */}
-        {[...Array(3)].map((_, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
-            <Paper elevation={0} sx={{ padding: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography variant="h6" component="h3" gutterBottom>
-                {`Opportunity ${index + 1}`}
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Description for opportunity {index + 1}. Summarize the details here.
-              </Typography>
-              <Button variant="contained" color="primary" sx={{ marginTop: 'auto' }}>
-                Learn More
-              </Button>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
     </Box>
   );
 }
